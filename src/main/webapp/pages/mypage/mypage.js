53b3ec4eebcb08d867000efebfaef3bf70a05fd8
Kishore Routhu <kishore.routhu@wavemaker.com>
2017-11-09 11:44:03 +0000

    New files added - [mypage,googleapis] Files modified - [profiles,webapp,pom.xml,project-services.xml,pages-config.json] New files added - [mypage,googleapis] Files modified - [profiles,webapp,pom.xml,project-services.xml,pages-config.json]

---
+++ src/main/webapp/pages/mypage/mypage.js
@@ -0,0 +1,14 @@
+Application.$controller("mypagePageController", ["$scope", function ($scope) {
+    "use strict";
+
+    /* perform any action on widgets within this block */
+    $scope.onPageReady = function () {
+        /*
+         * widgets can be accessed through '$scope.Widgets' property here
+         * e.g. to get value of text widget named 'username' use following script
+         * '$scope.Widgets.username.datavalue'
+         */
+    };
+
+}]);
+
